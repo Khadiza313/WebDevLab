bb9872c9e9554a44567006d318a96c42d79f8210
Khadizatul Kobra <khadizatulkobra313@gmail.com>
2026-07-22 12:28:57 +0600

    first commit

---
+++ part1/chapter1.js
@@ -0,0 +1,11 @@
+//1
+console.log("My name is:Kobra");
+console.log("Depeartment:CSE");
+console.log("University:Metropoliton University");
+
+//2
+//print hello world
+console.log("Hello, World!");
+
+//3
+console.log("kobita);
---
+++ part1/chapter2.js
@@ -0,0 +1,20 @@
+//1
+const name ="Kobra";
+const depeartment = "CSE";
+let semister = 4.1;
+console.log(`My name is ${name}, I am in the ${depeartment} depeartment,and i am in the ${semister} semister`); 
+
+//2
+let value = null;
+console.log(typeof value);
+
+
+//3
+const input= "19.5";
+const n = Number(input);
+console.log(n + 0.5);
+
+//4
+//"5"+3="53", "5"-3=2
+ console.log("5" +3);
+ console.log("5" -3);
